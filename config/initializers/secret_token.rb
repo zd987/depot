b7aa15b1612ae185a1a73ead49afa70aea989840
Dong Zhao <zhaodong8701@gmail.com>
2014-01-22 23:21:57 +0800

    add ror files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'cd926ca3cd36aec21ad4920a2a43d0014464831137b6f9defdfaccd7c82e1b8adf9f2317448d00b791a72715ce9e06e53f8834ccae6a9dc56bab6c8faf431c80'
